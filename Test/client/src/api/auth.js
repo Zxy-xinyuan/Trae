9001e9c46ff4bb50077214b676e8082e1bc69fa1
Zxy-xinyuan <3464059006@qq.com>
2026-06-26 13:42:09 +0800

    Delete Test directory

--- Test/client/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import http from './index'
-
-export function login(data) {
-  return http.post('/auth/login', data)
-}
-
-export function getUserInfo() {
-  return http.get('/auth/me')
-}
--- Test/client/src/api/settings.js
+++
@@ -1,9 +0,0 @@
-import http from './index'
-
-export function changePassword(data) {
-  return http.put('/settings/password', data)
-}
-
-export function publishNotify(data) {
-  return http.post('/settings/notify', data)
-}
